96bcc1f3ac5a477ce120b98a2d8964c46fdeaea2
pop-ketle <popqjim@gmail.com>
2020-03-05 08:59:06 +0900

    タイムスタンプに応じてサインはで移動する機能を追加 002分の更新

--- stg/001/script/script.js
+++ stg/001/script/script.js
@@ -29,6 +29,11 @@
      * @type {Image}
      */
     let image = null;
+    /** 
+     * 実行開始時のタイムスタンプ
+     * @type {number
+     */
+    let startTime = null;
 
     /**
      * ページのロードが完了した時に発火する loadイベント
@@ -45,6 +50,8 @@
         util.imageLoader('./image/viper.png', (loadedImage) => {
             // 引数経由で画像を受け取り変数に代入しておく
             image = loadedImage;
+            // 実効開始時のタイムスタンプを取得する
+            startTiem = Date.now();
             // 初期化処理を行う
             initialize();
             // 描画処理を行う
@@ -67,8 +74,20 @@
     function render(){
         // 描画前に画面全体を不透明な明るいグレーで塗りつぶす
         util.drawRect(0,0,canvas.width,canvas.height,'#eeeee');
-        // 画像を描画する
-        ctx.drawImage(image,100,100);
+
+        // 現在までの経過時間を取得する(ミリ秒を秒に変換するため1000で除算)
+        let nowTime = (Date.now() - startTime) / 1000;
+        // 時間の経過が見た目でわかりやすいように自機をサイン波で動かす
+        let s = Math.sin(nowTime);
+        // サインやコサインは半径1の円を基準にしているので、得られる値の範囲が
+        // -1.0 ~ 1.0になるため、効果がわかりやすくなるように100倍する
+        let x = s*100.0;
+
+        // 画像を描画する(canvasの中心位置を基準にサイン波で左右に往復するようにする)
+        ctx.drawImage(image,CANVAS_WIDTH/2 + x, CANVAS_HEIGHT/2);
+
+        // 工場ループのために描画処理を再帰呼び出しする
+        requestAnimationFrame(render);
     }
 
     /**
