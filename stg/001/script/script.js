0fe774c352312d5b4ac6b0b27c5eb0b345aaaa70
pop-ketle <popqjim@gmail.com>
2020-03-05 09:29:33 +0900

    キーボード操作で動くように変更 003分の更新

--- stg/001/script/script.js
+++ stg/001/script/script.js
@@ -34,6 +34,13 @@
      * @type {number
      */
     let startTime = null;
+    /**
+     * viperのX,Y座標
+     * @type {number}
+     * @type {number}
+     */
+    let viperX = CANVAS_WIDTH/2; // ここでは仮でcanvasの中心位置
+    let viperY = CANVAS_HEIGHT/2; // ここでは仮でcanvasの中心位置
 
     /**
      * ページのロードが完了した時に発火する loadイベント
@@ -50,10 +57,12 @@
         util.imageLoader('./image/viper.png', (loadedImage) => {
             // 引数経由で画像を受け取り変数に代入しておく
             image = loadedImage;
-            // 実効開始時のタイムスタンプを取得する
-            startTiem = Date.now();
             // 初期化処理を行う
             initialize();
+            // イベントを設定する
+            eventSetting();
+            // 実効開始時のタイムスタンプを取得する
+            startTime = Date.now();
             // 描画処理を行う
             render();
         });
@@ -69,22 +78,41 @@
     }
 
     /**
+     * イベントを設定
+     */
+    function eventSetting(){
+        // キーの押下時に呼び出されるイベントリスナーを設定する
+        window.addEventListener('keydown', (event) => {
+            // 入力されたキーに応じて処理内容を変化させる
+            switch(event.key){
+                case 'ArrowLeft':
+                    viperX-=10;
+                    break;
+                case 'ArrowRight':
+                    viperX+=10;
+                    break;
+                case 'ArrowUp':
+                    viperY-=10;
+                    break;
+                case 'ArrowDown':
+                    viperY+=10;
+                    break;
+            }
+        },false);
+    }
+
+    /**
      * 描画処理を行う
      */
     function render(){
         // 描画前に画面全体を不透明な明るいグレーで塗りつぶす
-        util.drawRect(0,0,canvas.width,canvas.height,'#eeeee');
+        util.drawRect(0,0,canvas.width,canvas.height,'#eeeeee');
 
         // 現在までの経過時間を取得する(ミリ秒を秒に変換するため1000で除算)
         let nowTime = (Date.now() - startTime) / 1000;
-        // 時間の経過が見た目でわかりやすいように自機をサイン波で動かす
-        let s = Math.sin(nowTime);
-        // サインやコサインは半径1の円を基準にしているので、得られる値の範囲が
-        // -1.0 ~ 1.0になるため、効果がわかりやすくなるように100倍する
-        let x = s*100.0;
 
         // 画像を描画する(canvasの中心位置を基準にサイン波で左右に往復するようにする)
-        ctx.drawImage(image,CANVAS_WIDTH/2 + x, CANVAS_HEIGHT/2);
+        ctx.drawImage(image,viperX,viperY);
 
         // 工場ループのために描画処理を再帰呼び出しする
         requestAnimationFrame(render);
